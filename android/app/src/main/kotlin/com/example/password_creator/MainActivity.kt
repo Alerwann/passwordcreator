8578a6d1c4e1d6399a413b4af6b408aa214025e2
Alerwann <alerwann411@gmail.com>
2025-11-29 22:34:10 +0100

    feat: enable edge-to-edge display in MainActivity by configuring system windows.

--- android/app/src/main/kotlin/com/example/password_creator/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/password_creator/MainActivity.kt
@@ -1,5 +1,15 @@
 package com.alerwann.password_robust_creator
 
 import io.flutter.embedding.android.FlutterActivity
+import android.os.Bundle
+import androidx.core.view.WindowCompat // Nouvel import requis pour le Edge-to-Edge
 
-class MainActivity : FlutterActivity()
+class MainActivity : FlutterActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        // Définir la fenêtre pour dessiner derrière les barres système.
+        // C'est l'étape nécessaire pour l'affichage Edge-to-Edge.
+        WindowCompat.setDecorFitsSystemWindows(window, false) 
+        
+        super.onCreate(savedInstanceState)
+    }
+}
